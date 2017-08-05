004bf8d861874fc590230fc3a33e241a9fd23723
Jeremy Castillo <jcast029.jc@gmail.com>
2017-08-05 08:57:35 -0700

    Added new fonts and styling. Working on Signup page.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const hbs = require('hbs');
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials/common');
+hbs.registerPartials(__dirname + '/views/partials/account');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
@@ -12,6 +13,16 @@ app.get("/", (req, res) => {
     headingTitle: 'MyAlmanac',
     content: 'home',
     home: true
+  });
+});
+
+app.get("/signup", (req, res) => {
+  
+
+  res.render('template.hbs', {
+    headingTitle: 'MyAlmanac: Log In',
+    content: 'signup',
+    home: false
   });
 });
 
